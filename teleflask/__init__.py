73d6bdb9fcf0693642d2f33b15c8bc7988b97588
luckydonald <mac2019.git@luckydonald.de>
2020-06-29 23:08:22 +0200

    filter_rewrite: Speaking of top level import.

--- teleflask/__init__.py
+++ teleflask/__init__.py
@@ -9,14 +9,18 @@ __version__ = VERSION
 __all__ = [
     'VERSION', '__version__',
     # the ones we provide for easy toplevel access:
-    'Teleserver', 'Teleflask', 'TBlueprint', 'abort_processing',
+    'Teleprocessor', 'Teleserver',
+    'TBlueprint', 'abort_processing',
+    # special modules:
+    'Teleflask',
     # submodules:
    'server', 'exceptions', 'messages', 'new_messages', 'proxy',
 ]
 
-from .server.core import Teleserver
+from .server.core import Teleprocessor, Teleserver
 
 if _sys.version_info.major >= 3 and _sys.version_info.minor >= 6:
+    IMPORTS = {'Teleflask': '.server.extras.flask',}
     try:
         # we try to serve them as lazy imports
         import importlib as _importlib
@@ -24,10 +28,10 @@ if _sys.version_info.major >= 3 and _sys.version_info.minor >= 6:
         _module = _importlib.import_module('.sever', __name__)
 
         def __getattr__(name):
-            if name not in ('Teleserver', 'Teleflask'):
-                raise AttributeError(f'module {_module.__spec__.parent!r} has no attribute {name!r}')
+            if name not in IMPORTS:
+                raise AttributeError(f'module {_module.__spec__.parent!r} has no attribute {name!r} ({IMPORTS[name]!r}')
             # end if
-            imported = _importlib.import_module(name, _module.__spec__.parent)
+            imported = _importlib.import_module(IMPORTS[name], _module.__spec__.parent)
             return imported
         # end def
     except Exception:
